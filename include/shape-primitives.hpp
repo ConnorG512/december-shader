9538b798aafc4625b187a79c27c78cfdf79f95ce
Connor Garey <connorgarey1999@gmail.com>
2025-12-06 22:12:11 +0000

    Added ShapePrimitives and using it in the render.

---
+++ include/shape-primitives.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <array>
+
+namespace Shapes {
+
+  inline constexpr std::array<float, 9> triangle {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  };
+}
