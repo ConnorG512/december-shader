fe0fb5632df9bdb2068b7727347882fd18ff3f30
Connor Garey <connorgarey1999@gmail.com>
2025-12-06 22:33:46 +0000

    Changed to unsigned.

--- include/shape-primitives.hpp
+++ include/shape-primitives.hpp
@@ -3,10 +3,14 @@
 #include <array>
 
 namespace Shapes {
-
   inline constexpr std::array<float, 9> triangle {
     -0.5f, -0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
     0.0f, 0.5f, 0.0f
+  };
+  
+  inline constexpr std::array<std::uint32_t, 6> rectangle {
+    0, 1, 2,
+    1, 2, 3,
   };
 }
