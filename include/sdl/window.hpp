e2cafcc5fd82fe4fb7d8cf94ea8cfdf1db144ca9
Connor Garey <connorgarey1999@gmail.com>
2025-12-07 14:16:03 +0000

    Spans a window that cannot be resized and now the rectangle fills the full screen.

--- include/sdl/window.hpp
+++ include/sdl/window.hpp
@@ -11,5 +11,5 @@ public:
 
 private:
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window_instance_{
-      SDL_CreateWindow("December Shader", 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE), &SDL_DestroyWindow};
+      SDL_CreateWindow("December Shader", 1280, 720, SDL_WINDOW_OPENGL ), &SDL_DestroyWindow};
 };
