ea92f4cf9910813de14c5620a21afd1ffbc0c7c3
Connor Garey <connorgarey1999@gmail.com>
2025-12-07 20:45:53 +0000

    Implementation for drawing mesh.

--- src/opengl/mesh.cpp
+++ src/opengl/mesh.cpp
@@ -3,13 +3,17 @@
 #include "extern/glad/glad.h"
 
 Mesh::Mesh(std::span<const float> verticies)
+  : mesh_{verticies}
 {
   glGenVertexArrays(1, &VAO_);
   glGenBuffers(1, &VBO_);
   
   glBindVertexArray(VAO_);
   glBindBuffer(GL_ARRAY_BUFFER, VBO_);
-  glBufferData(GL_ARRAY_BUFFER, verticies.size() * sizeof(float), verticies.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, mesh_.size() * sizeof(float), mesh_.data(), GL_STATIC_DRAW);
+  
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
 }
 
 Mesh::~Mesh()
@@ -21,4 +25,9 @@ Mesh::~Mesh()
 auto Mesh::bindVAO() noexcept -> void
 {
     glBindVertexArray(VAO_);
+}
+
+auto Mesh::draw() noexcept -> void 
+{
+  glDrawArrays(GL_TRIANGLES, 0, mesh_.size() / 3);
 }
