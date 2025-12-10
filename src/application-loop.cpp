75da388949d40c9c37b18f73d4ea6b6679ce3b2f
Connor Garey <connorgarey1999@gmail.com>
2025-12-10 20:34:29 +0000

    Squash merge rewrite into gpu-buffer

--- src/application-loop.cpp
+++ src/application-loop.cpp
@@ -8,30 +8,6 @@
 App::MainLoop::MainLoop()
 {
   // Background: 
-  background_program_ = glCreateProgram();
-  glAttachShader(background_program_, background_vertex.Id());
-  glAttachShader(background_program_, background_fragment.Id());
-  glLinkProgram(background_program_);
-  
-  // Can be deleted after it has been linked.
-  glDeleteShader(background_vertex.Id());
-  glDeleteShader(background_fragment.Id());
-
-  // Binding rectangle
-  gpu_buffer_.bindData(std::span<const float>(Shapes::rectangle));
-  
-  // Background: 
-  triangle_program_ = glCreateProgram();
-  glAttachShader(triangle_program_, triangle_vertex.Id());
-  glAttachShader(triangle_program_, triangle_fragment.Id());
-  glLinkProgram(triangle_program_);
-  
-  // Can be deleted after it has been linked.
-  glDeleteShader(triangle_vertex.Id());
-  glDeleteShader(triangle_fragment.Id());
-
-  // Binding rectangle
-  gpu_buffer_.bindData(std::span<const float>(Shapes::triangle));
 }
 
 auto App::MainLoop::run() -> void 
@@ -50,14 +26,6 @@ auto App::MainLoop::run() -> void
       // Main Loop.
       glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
-
-      glUseProgram(background_program_);
-      background_mesh.bindVAO();
-      background_mesh.draw();
-      
-      glUseProgram(triangle_program_);
-      triangle_mesh.bindVAO();
-      triangle_mesh.draw();
 
       current_window_.swapWindow();
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,138 @@
 #include "sdl/window.hpp"
-#include "application-loop.hpp"
+#include "extern/glad/glad.h"
+#include "shape-primitives.hpp"
+#include "opengl/opengl.hpp"
 
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_time.h>
+#include <print>
 
 auto main() -> int
 {
   SDL_SetAppMetadataProperty("December Shader", "1.0");
-  App::MainLoop application_loop{};
   
-  application_loop.run();
+  // Variables 
+  Window current_window{};
+
+  // Setup
+  // Rectangle
+  
+  // Creating a VBO: 
+  std::uint32_t VBO_rectangle {};
+  OGL::setupVBO(VBO_rectangle);
+  
+  // Vertex Shader:
+  std::uint32_t vertex_shader_rectangle{glCreateShader(GL_VERTEX_SHADER)};
+  auto shader_data_rectangle{FileOperations::readToMemory("data/background.vert")};
+  if(!shader_data_rectangle.has_value())
+  {
+    std::println("Failed to read shader file! {}", shader_data_rectangle.error());
+    return EXIT_FAILURE;
+  }
+  const char* shader_source_rectangle{reinterpret_cast<const char*>(shader_data_rectangle->data())};
+  glShaderSource(vertex_shader_rectangle, 1, &shader_source_rectangle, NULL);
+  glCompileShader(vertex_shader_rectangle);
+
+  // Fragment Shader
+  std::uint32_t fragment_shader_rectangle{glCreateShader(GL_FRAGMENT_SHADER)};
+  auto frag_shader_data_rectangle{FileOperations::readToMemory("data/background.frag")};
+  if(!frag_shader_data_rectangle.has_value())
+  {
+    std::println("Failed to read shader file! {}", shader_data_rectangle.error());
+    return EXIT_FAILURE;
+  }
+  const char* frag_shader_source_rectangle{reinterpret_cast<const char*>(frag_shader_data_rectangle->data())};
+  glShaderSource(fragment_shader_rectangle, 1, &frag_shader_source_rectangle, NULL);
+  glCompileShader(fragment_shader_rectangle);
+
+  // Shader Program
+  std::uint32_t shader_program_rectangle{glCreateProgram()};
+  OGL::attachAndLinkShader({vertex_shader_rectangle, fragment_shader_rectangle}, shader_program_rectangle);
+  glUseProgram(shader_program_rectangle);
+  
+  // VAO_rectangle:
+  std::uint32_t VAO_rectangle {};
+  glGenVertexArrays(1, &VAO_rectangle);
+  glBindVertexArray(VAO_rectangle);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_rectangle);
+  glBufferData(GL_ARRAY_BUFFER, Shapes::rectangle.size() * sizeof(float), Shapes::rectangle.data(), GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // Triangle
+  // Creating a VBO: 
+  std::uint32_t VBO_Triangle {};
+  OGL::setupVBO(VBO_Triangle);
+  
+  // Vertex Shader:
+  std::uint32_t vertex_shader_triangle{glCreateShader(GL_VERTEX_SHADER)};
+  auto shader_data{FileOperations::readToMemory("data/triangle.vert")};
+  if(!shader_data.has_value())
+  {
+    std::println("Failed to read shader file! {}", shader_data.error());
+    return EXIT_FAILURE;
+  }
+  const char* shader_source{reinterpret_cast<const char*>(shader_data->data())};
+  glShaderSource(vertex_shader_triangle, 1, &shader_source, NULL);
+  glCompileShader(vertex_shader_triangle);
+
+  // Fragment Shader
+  std::uint32_t fragment_shader_triangle{glCreateShader(GL_FRAGMENT_SHADER)};
+  auto frag_shader_data{FileOperations::readToMemory("data/triangle.frag")};
+  if(!frag_shader_data.has_value())
+  {
+    std::println("Failed to read shader file! {}", shader_data.error());
+    return EXIT_FAILURE;
+  }
+  const char* frag_shader_source{reinterpret_cast<const char*>(frag_shader_data->data())};
+  glShaderSource(fragment_shader_triangle, 1, &frag_shader_source, NULL);
+  glCompileShader(fragment_shader_triangle);
+
+  // Shader Program
+  std::uint32_t shader_program_triangle{glCreateProgram()};
+  OGL::attachAndLinkShader({vertex_shader_triangle, fragment_shader_triangle}, shader_program_triangle);
+  glUseProgram(shader_program_triangle);
+  
+  // VAO:
+  std::uint32_t VAO {};
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Triangle);
+  glBufferData(GL_ARRAY_BUFFER, Shapes::triangle.size() * sizeof(float), Shapes::triangle.data(), GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  OGL::deleteShaders({vertex_shader_triangle, fragment_shader_triangle, vertex_shader_rectangle, fragment_shader_rectangle});
+  // Loop
+  bool is_running {true};
+  while(is_running)
+  {
+    SDL_Event event{};
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_EVENT_QUIT)
+      {
+        is_running = false;
+      }
+      
+    }
+
+    // Main Loop.
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program_rectangle);
+    glBindVertexArray(VAO_rectangle);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glUseProgram(shader_program_triangle);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    current_window.swapWindow();
+  }
   
   return EXIT_SUCCESS;
 }
-    //Getting Time for the phasing colour:
-    //std::uint64_t current_time {SDL_GetTicks() / 1000};
-    //std::int32_t time_location {glGetUniformLocation(shader_program, "time")};
-    //glUniform1f(time_location, current_time);
-    
-    // Logic
-
